ab4538ebea0c3b18eb72332513296771fbeaa43c
takahashiyuji <darking7890@gmail.com>
2018-05-10 09:38:04 +0900

    画像追加

--- app/src/main/java/filedonwloadertakhashiyuji/abs/co/jp/downloader/Top.java
+++ app/src/main/java/filedonwloadertakhashiyuji/abs/co/jp/downloader/Top.java
@@ -31,10 +31,10 @@ public class Top extends AppCompatActivity {
     }
     public void onClick(View v) {
         EditText text =findViewById(R.id.editText3);
-        ImageView image=findViewById(R.id.imageView);
+        ImageView image=findViewById(R.id.DownloadImage);
         if(text.toString()==REQUEST_CODE){
+            Toast.makeText(this, "Toast example", Toast.LENGTH_LONG).show();
+            image.setImageResource(R.drawable.main);
         }
-        Toast.makeText(this, "Toast example", Toast.LENGTH_LONG).show();
-
-    }
+      }
     }
