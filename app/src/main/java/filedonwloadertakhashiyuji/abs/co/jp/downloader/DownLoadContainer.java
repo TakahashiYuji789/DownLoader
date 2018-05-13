31e232f203c40efbdcfc1b3d1c45a43f0bcf78b1
takahashiyuji <darking7890@gmail.com>
2018-05-13 16:05:38 +0900

    Revert "Revert "試しに使ったプログラム""
    This reverts commit 0e2ccca6b5bf03bf78c51758d34fb2dfa312a1d0.

--- app/src/main/java/filedonwloadertakhashiyuji/abs/co/jp/downloader/DownLoadContainer.java
+++ app/src/main/java/filedonwloadertakhashiyuji/abs/co/jp/downloader/DownLoadContainer.java
@@ -5,7 +5,7 @@ import android.os.AsyncTask;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DownLoadContainer  extends AsyncTask<Void, Void, Void> {
+public class DownLoadContainer   {
     //ダウンロードデータ
     private DownLoadData data;
     //リクエストコード
@@ -13,6 +13,7 @@ public class DownLoadContainer  extends AsyncTask<Void, Void, Void> {
     //コールバックの引数に渡す結果データ
     private Map<String, Object> resultMap;
 
+    public  boolean Status;
     //コンストラクタ
     public DownLoadContainer(final DownLoadData downLoadData, final String requestCode){
         this.data = downLoadData;
@@ -20,20 +21,4 @@ public class DownLoadContainer  extends AsyncTask<Void, Void, Void> {
         resultMap = new HashMap<String,Object>();
     }
 
-    /**
-     * executeが実行された後に実行される。
-     */
-    @Override
-    protected Void doInBackground(Void... params) {
-        // DB登録等のUIに関与しない処理
-        // resuletMapにActivityに渡した値を詰めたり
-        return null;
-    }
-
-    /**
-     * コールバックメソッドを実行する。
-     */
-    protected void onPostExecute(Void... params) {
-        data.callBack(DownLoadData.OK, 0, resultMap);
-    }
 }
