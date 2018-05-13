63e00405dd7a037dc18e55c18d3caa263e66eafd
takahashiyuji <darking7890@gmail.com>
2018-05-13 15:59:49 +0900

    5/13

--- app/src/main/java/filedonwloadertakhashiyuji/abs/co/jp/downloader/DownLoadContainer.java
+++ app/src/main/java/filedonwloadertakhashiyuji/abs/co/jp/downloader/DownLoadContainer.java
@@ -5,7 +5,7 @@ import android.os.AsyncTask;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DownLoadContainer   {
+public class DownLoadContainer  extends AsyncTask<Void, Void, Void> {
     //ダウンロードデータ
     private DownLoadData data;
     //リクエストコード
@@ -13,7 +13,6 @@ public class DownLoadContainer   {
     //コールバックの引数に渡す結果データ
     private Map<String, Object> resultMap;
 
-    public  boolean Status;
     //コンストラクタ
     public DownLoadContainer(final DownLoadData downLoadData, final String requestCode){
         this.data = downLoadData;
@@ -21,4 +20,20 @@ public class DownLoadContainer   {
         resultMap = new HashMap<String,Object>();
     }
 
+    /**
+     * executeが実行された後に実行される。
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+        // DB登録等のUIに関与しない処理
+        // resuletMapにActivityに渡した値を詰めたり
+        return null;
+    }
+
+    /**
+     * コールバックメソッドを実行する。
+     */
+    protected void onPostExecute(Void... params) {
+        data.callBack(DownLoadData.OK, 0, resultMap);
+    }
 }
